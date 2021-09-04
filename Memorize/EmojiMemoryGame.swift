41af3383333c40435fa00dbd8b36da5750ba0e7f
Jimin Kim <0305jimin@gmail.com>
2021-09-04 13:23:33 +0900

    Feat: completing ViewModel

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -8,10 +8,23 @@
 import SwiftUI
 
 class EmojiMemoryGame {
-    var model: MemoryGame<String> = MemoryGame<String>(numberOfPairsOfCards: 4) {_ in "🥰" }
-    //Swift already knows that createCardContent get Int argument and return String, so I don't have to state the types. also earase the parentheses around 'index'. also 'return'
-    //as the createCardContent is the last argument??? I can erase the name of the argument and take the content out of the paretheses
-    //I don't use 'index' so this can be '_'
+    
+   static let emojis = ["😃","😄","😁" ,"😊","😋","🤪","🥰","☺️","🥳","😏","😎","😭","🥺","😡","😱","🤫","😬","🙄","😪","🤩","🥶","😈","🤣","😘"]
+    
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+            emojis[pairIndex]
+        }
+    }
+    
+    private var model: MemoryGame<String> = createMemoryGame()//When initializing or using inside static type function, I don't have to put EmojiMemoryGame. in front of createMemeoryGame, emojis. 
+        
+    
+    //'Cannot use instance member 'emojis' within property initializer; property initializers run before 'self' is available'
+    //instance memver: var, let, func. property: var, let inside the class
+    //the order of initializing properties is random.
+    //resolution 1. make emojis as global variable -but using global variable isn't great way. 2. put 'static' in front of 'let'
     
     var cards: Array<MemoryGame<String>.Card> {
         return model.cards

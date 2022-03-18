b3ca70837907dff1759222b6f75e4e90d52ecb8e
Jimin Kim <0305jimin@gmail.com>
2022-03-18 19:46:38 +0900

    [SwiftBasics] print, var, let

---
+++ swiftBasics/swiftBasics/main.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  swiftBasics
+//
+//  Created by 김지민 on 2022/03/18.
+//
+
+import Foundation
+
+// variable
+var name="Jimin"
+name="Jimin Kim"
+
+// constant
+let greetingMessage = "Nice to meet you ! 😆"
+let first_name = "Jimin", last_name = "Kim"
+/* constant and variable names : almost any character, including Unicode characters
+❌ blank space
+❌ mathematical symbols
+❌ -
+❗️ numbers can appear anywhere within a name except for at the beginning. ...*/
+
+
+//printing texts
+print("Hello")
+print("my name is \(name)")
+print(greetingMessage)
+
+/*Use of the semicolon (;) following each statement is fully optional in Swift,
+unless you wish to place several independent statements on a single line, in which case the semi-colon is required.*/
